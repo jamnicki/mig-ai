d99aa44fb29007b0d82b7c79354d919198e3bfe4
Jedrzej Jamnicki <jedrzej.jamnicki@gmail.com>
2023-10-19 14:03:14 +0000

    feat: simple annotation eda

--- src/settings.py
+++ src/settings.py
@@ -13,3 +13,13 @@ DATA_DIR = ROOT / "data"
 ORKPJM_DIR = DATA_DIR / "ORKPJM"
 ORKPJM_VIDEOS_DIR = ORKPJM_DIR / "filmy_ORKPJM"
 ORKPJM_ANN_DIR = ORKPJM_DIR / "eafy_CZERWIEC2021"
+
+
+TASK_NUM_NAME_MAP = {
+    "7": "Kalendarz",
+    "8": "Sylwester / Żaba",
+    "13": "Komisky",
+    "15": "Znaki zakazu",
+    "17": "Gruszki / Chaplin",
+    "24": "Alarm",
+}
